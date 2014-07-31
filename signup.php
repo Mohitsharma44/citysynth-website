ec444c636d60be8c2fc9377332455f6bf1abd80e
Mohitsharma44 <Mohitsharma44@gmail.com>
2014-07-31 10:49:09 -0400

    signup

--- signup.php
+++ signup.php
@@ -178,7 +178,7 @@
           <div class="row">
             <div class="col-md-2">
             </div>
-            <div class="col-md-5">
+            <div class="col-md-10">
               <button type="submit" class="btn btn-success">
                 Register
               </button>
@@ -187,6 +187,39 @@
         </form>
       </div>
     </div>
+
+    <div class="container" style="margin: 10px; float:right; width: 50%;">
+      <form class="form-horizontal" role="form">
+        <div class="form-group">
+          <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+          <div class="col-sm-10">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+          <div class="col-sm-10">
+            <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-offset-2 col-sm-10">
+            <div class="checkbox">
+              <label>
+                <input type="checkbox"> Remember me
+              </label>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-success">Sign in</button>
+            <a href = "./signup.php" class="navbar-btn btn btn-primary">Register</a>
+          </div>
+        </div>
+      </form>
+    </div>
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery.js"></script>
 
