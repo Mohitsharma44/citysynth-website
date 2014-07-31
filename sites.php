43152aa1ad95a3ad461d0d3380066a2101a68c7a
Mohitsharma44 <Mohitsharma44@gmail.com>
2014-07-31 14:11:28 -0400

    sites

--- sites.php
+++ sites.php
@@ -97,132 +97,133 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 <body>
 
+  <style type="text/css">
+
+    div.c-wrapper{
+      width: 80%; /* for example */
+      margin: 0;
+    }
+
+    p.italic {font-style:italic;float: left;}
+
+  </style>
+
+  <div class="navbar navbar-inverse default navbar-fixed-top">
+    <!--If the top bar moves, change static to fixed and at the end of this dic class, adjust the next div class with some padding-->
     <style type="text/css">
-
-      div.c-wrapper{
-        width: 80%; /* for example */
-        margin: 0;
+      p{
+        margin: 0px 0px 0px 0px;
       }
-
-      p.italic {font-style:italic;float: left;}
-
+      div{
+        margin: 0px 0px 0px 0px;
+      }
     </style>
-
-    <div class="navbar navbar-inverse default navbar-fixed-top">
-      <!--If the top bar moves, change static to fixed and at the end of this dic class, adjust the next div class with some padding-->
-      <style type="text/css">
-        p{
-          margin: 0px 0px 0px 0px;
-        }
-        div{
-          margin: 0px 0px 0px 0px;
-        }
-      </style>
-      <div class="container">
-        <a href="./index.php" class="navbar-brand"><p style="font-size: 22px;font-weight: 500;font-family: 'Open Sans', sans-serif; align="left" ">CITYSYNTH</br></p><p style="font-size:12px; align="left" ">&nbsp;THE URBAN GALAXY</p></a>
-        <button class = "navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
-          <span class = icon-bar></span>
-          <span class = icon-bar></span>
-          <span class = icon-bar></span>
-        </button>
+    <div class="container">
+      <a href="./index.php" class="navbar-brand"><p style="font-size: 22px;font-weight: 500;font-family: 'Open Sans', sans-serif; align="left" ">CITYSYNTH</br></p><p style="font-size:12px; align="left" ">&nbsp;THE URBAN GALAXY</p></a>
+      <button class = "navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
+        <span class = icon-bar></span>
+        <span class = icon-bar></span>
+        <span class = icon-bar></span>
+      </button>
 
 
-        <div class="collapse navbar-collapse navHeaderCollapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li>
-              Welcome,
-              <?php
-              if (isset($_SESSION['user']))
-                echo "<a>Hello ".$_SESSION['user']."</a>"."<a href='logout.php'>Logout</a>";
-              ?>
+      <div class="collapse navbar-collapse navHeaderCollapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            Welcome,
+            <?php
+            if (isset($_SESSION['user']))
+              echo "<a>Hello ".$_SESSION['user']."</a>"."<a href='logout.php'>Logout</a>";
+            ?>
 
-            </li>
-            <li class="active"><a href="./index.php">Home</a></li>
-            <li><a href="#myModal" data-toggle="modal">Login/ Signup</a></li>
-            <li class="dropdown">
-              <a href="#" class = "dropdown-toggle" data-toggle = "dropdown">Time Lapse Videos <b class="caret"></b></a>
-              <ul class = "dropdown-menu">
-                <li><a href="#">Manhattan</a></li>
-                <li><a href="#">Brooklyn</a></li>
-              </ul></li>
-              <li><a href="#">Contact Us</a></li>
-            </ul>
-          </div>
-
-        </div>
-      </div> 
-
-      <div class = "navbar navbar-default navbar-fixed-bottom">
-
-        <div class = "container">
-
-          <p class="navbar-text pull-left">Site Built by Mohit</p>
-          <a href = "./signup.php" class="navbar-btn btn-success btn pull-right">Volunteer with us!</a>
-
+          </li>
+          <li class="active"><a href="./index.php">Home</a></li>
+          <li><a href="#myModal" data-toggle="modal">Login/ Signup</a></li>
+          <li class="dropdown">
+            <a href="#" class = "dropdown-toggle" data-toggle = "dropdown">Time Lapse Videos <b class="caret"></b></a>
+            <ul class = "dropdown-menu">
+              <li><a href="#">Manhattan</a></li>
+              <li><a href="#">Brooklyn</a></li>
+            </ul></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
         </div>
 
       </div>
+    </div> 
 
-      <!-- Modal -->
-      <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-              <h4 class="modal-title" id="myModalLabel">Sign In to Citysynth</h4>
-            </div>
-            <div class="modal-body">
-              <form class="form-horizontal" role="form" method="post" action="add.php?entity=login" name="aform" target="_top">
-                <div class="form-group">
-                  <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-                  <div class="col-sm-10">
-                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-                  </div>
+    <div class = "navbar navbar-default navbar-fixed-bottom">
+
+      <div class = "container">
+
+        <p class="navbar-text pull-left">Site Built by Mohit</p>
+        <a href = "./signup.php" class="navbar-btn btn-success btn pull-right">Volunteer with us!</a>
+
+      </div>
+
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+            <h4 class="modal-title" id="myModalLabel">Sign In to Citysynth</h4>
+          </div>
+          <div class="modal-body">
+            <form class="form-horizontal" role="form" method="post" action="add.php?entity=login" name="aform" target="_top">
+              <div class="form-group">
+                <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                <div class="col-sm-10">
+                  <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
                 </div>
-                <div class="form-group">
-                  <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-                  <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-                  </div>
-                </div>
-                <div class="form-group">
-                  <div class="col-sm-offset-2 col-sm-10">
-                    <div class="checkbox">
-                      <label>
-                        <input type="checkbox"> Remember me
-                      </label>
-                    </div>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <div class="col-sm-offset-2 col-sm-10">
-                    <button type="submit" class="btn btn-success">Sign in</button>
-                    <a href = "./signup.php" class="navbar-btn btn btn-primary">Register</a>
-                  </div>
-                </div>
-              </form>
-            </div>
-            <div class="modal-footer">
-              <p class="italic">Signing in will give you access to the live feed <br/>from 
-                the cameras of other volunteers.</p>
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
               </div>
+              <div class="form-group">
+                <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <div class="checkbox">
+                    <label>
+                      <input type="checkbox"> Remember me
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <button type="submit" class="btn btn-success">Sign in</button>
+                  <a href = "./signup.php" class="navbar-btn btn btn-primary">Register</a>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <p class="italic">Signing in will give you access to the live feed <br/>from 
+              the cameras of other volunteers.</p>
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
             </div>
           </div>
         </div>
+      </div>
 
 
+      <div style="margin: 20px">
+        <?php 
+        if (!isset($_SESSION['user']))
+          echo"<h2><a href='signup.php'>Login </a> to see the map</h2>";
+        else
+        {
+          ?>
 
-  <?php 
-  if (!isset($_SESSION['user']))
-    echo"<h2><a href='signup.php'>Login </a> to see the map</h2>";
-  else
-  {
-    ?>
+          <div style="width:48%;float:left" id="map-canvas">
+          </div>
 
-    <div style="width:48%;float:left" id="map-canvas">
-    </div>
-
-    <?php } ?>
-</body>
-</html>
+          <?php } ?>
+        </div>
+      </body>
+      </html>
