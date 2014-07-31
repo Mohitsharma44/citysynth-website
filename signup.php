39e9743f581631febd6860f89a5b7fbaf033e05a
Mohit <mohit@mohit-HP-ENVY-TouchSmart-Sleekbook-4.(none)>
2014-07-30 21:35:02 -0400

    php

--- signup.php
+++ signup.php
@@ -116,7 +116,7 @@
       <div class="col-md-2">
       </div>
       <div class="col-md-10">
-        <button type="submit" class="btn btn-info">
+        <button type="submit" class="btn btn-success">
           Register
         </button>
       </div>
