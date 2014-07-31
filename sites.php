29cbcc30ab95eb90dca57447a9f72995b36a6a62
Mohitsharma44 <Mohitsharma44@gmail.com>
2014-07-31 14:57:28 -0400

    sites

--- sites.php
+++ sites.php
@@ -195,7 +195,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
               <div class="form-group">
                 <div class="col-sm-offset-2 col-sm-10">
                   <form action="./sites.php">
-                    <button type="submit" class="btn btn-success">Sign in</button>
+                    <input type="submit" class="btn btn-success" value="Sign In">
                   </form>
                   <a href = "./signup.php" class="navbar-btn btn btn-primary">Register</a>
                 </div>
