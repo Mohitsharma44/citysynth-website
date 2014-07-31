d0eb94d24f6ea73944bd12365c90359fe51d5e44
Mohitsharma44 <Mohitsharma44@gmail.com>
2014-07-31 12:45:12 -0400

    signup

--- index.php
+++ index.php
@@ -109,7 +109,7 @@
 						<?php
 						if (isset($_SESSION['user']))
 							echo "<a>Hello ".$_SESSION['user']."</a>";
-							echo "<a href="./logout.php">Logout</a>";
+							echo "<a>Logout</a>";
 						?>
 					</li>
 					</ul>
