72f18dc07ce15fb42d5c746ef8dedc8d45099f4e
Mohitsharma44 <Mohitsharma44@gmail.com>
2014-07-31 13:13:49 -0400

    signup

--- sites.php
+++ sites.php
@@ -109,6 +109,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
     <div style="width:50%; height:100%;float:right" id="tp">
 
     </div> 
+    <?php } ?>
   </div>
 </body>
 </html>
