4143f5b1c308295f537f6fc8b8a4457cde4fa93d
Mohitsharma44 <Mohitsharma44@gmail.com>
2014-07-31 11:33:24 -0400

    signup

--- signup.php
+++ signup.php
@@ -3,16 +3,6 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <!-- Bootstrap -->
-  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-
-    <!--[if lt IE 9]>
-<script src="js/html5shiv.js"></script>
-<script src="js/respond.min.js"></script>
-<![endif]-->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>CitySynth</title>
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <link href="styles.css" rel="stylesheet">
