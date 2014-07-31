effebe9437c77110fd0852fc0951fc16e3ec8728
Mohitsharma44 <Mohitsharma44@gmail.com>
2014-07-31 16:01:07 -0400

    sites

---
+++ contactUs.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CitySynth: Register</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="styles.css" rel="stylesheet">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js">
+  </script>
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      
+    <section class="span6">Left</section>
+    <section class="span6">Right</section>
+    </div>
+
+  </div>
+
+</body>
+</html>
