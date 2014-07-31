3d70a8b1c20ad0eccfee5e1d6f6c684255f9fab0
Mohitsharma44 <Mohitsharma44@gmail.com>
2014-07-31 13:13:21 -0400

    signup

--- sites.php
+++ sites.php
@@ -104,12 +104,11 @@ google.maps.event.addDomListener(window, 'load', initialize);
   {
     ?>
 
-    <div style="width:48%;float:left" id="map-canvas" class="well">
+    <div style="width:48%;float:left" id="map-canvas">
     </div>
-    <div style="width:50%; height:100%;float:right" id="tp" class="well">
+    <div style="width:50%; height:100%;float:right" id="tp">
 
     </div> 
-    <?php } ?>
   </div>
 </body>
 </html>
