5438fd02913d926527aa71b37bf6e1bafeb7c0ec
Mohit <Mohitsharma44@gmail.com>
2014-08-06 21:37:40 -0400

    video

--- video.php
+++ video.php
@@ -1,28 +1,49 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Bootstrap video player</title>
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/video-default.css" rel="stylesheet">
-        <style>
-            body{padding-top: 10px}
-        </style>
-    </head>
-    <body>
-    <div class="videoUiWrapper thumbnail">
-  <video width="540" height="420" id="demo1">
-    <source src="videos/Manhattan.mp4" type="video/mp4">
-    Your browser does not support the video tag.
-  </video>
-</div>
-        <script type="text/javascript" src="js/jquery-1.8.1.min.js"></script>
-        <script type="text/javascript" src="js/jquery.video-ui.js"></script>
-        <script type="text/javascript" >
-            $('#demo1').videoUI({'autoplay' : true, 
-            	'playMedia' : true,
-            	'autohide' : false,
-            });
-        </script>
-    </body>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head>
+    <!-- This page is copyright Elated Communications Ltd. (www.elated.com) -->
+
+    <title>Timelapse Videos</title>
+
+<link rel="stylesheet" href="css/flexslider.css" type="text/css">
+   
+  </head>
+  <body>
+    <div class="tabContent" id="video1">
+      <h2>Manhattan Timelapse</h2>
+      <div>
+        <video style="margin-right: auto;margin-left: auto;" width="100%" height="70%" controls>
+  		<source src="videos/Manhattan.mp4" type="video/mp4">
+		</video>
+      </div>
+    </div>
+
+    <div class="tabContent hide" id="video2">
+      <h2>Header for video 2</h2>
+      <div>
+        <video style="margin-right: auto;margin-left: auto;" width="100%" height="70%" controls>
+  		<source src="videos/Manhattan.mp4" type="video/mp4">
+		</video>
+      </div>
+    </div>
+
+    <div class="tabContent hide" id="video3">
+      <h2>Header for video 3</h2>
+      <div>
+        <video style="margin-right: auto;margin-left: auto;" width="100%" height="70%" controls>
+  		<source src="videos/Manhattan.mp4" type="video/mp4">
+		</video>
+      </div>
+    </div>
+    <div class="tabContent hide" id="video4">
+      <h2>Header for video 4</h2>
+      <div>
+       <video style="margin-right: auto;margin-left: auto;" width="100%" height="70%" controls>
+  		<source src="videos/Manhattan.mp4" type="video/mp4">
+		</video>
+      </div>
+    </div>
+
+
+  
+
+</body>
 </html>
