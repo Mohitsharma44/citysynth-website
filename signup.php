ce296385052fd91ab3f0a04976883a5abf489148
Mohitsharma44 <Mohitsharma44@gmail.com>
2014-08-07 13:53:09 -0400

    upload image

--- signup.php
+++ signup.php
@@ -283,7 +283,7 @@
                   Upload Image:
                 </label>
                 <div class="col-md-10">
-                  <input name="file" type="file" name="file" id="file">
+                  <input name="file" type="file" name="file" id="file" accept="image/*">
                   <p class="help-block">
                     Allowed formats: jpeg, jpg, gif, png
                   </p>
